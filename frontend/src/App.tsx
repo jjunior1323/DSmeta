bbad7a7b3bb805756e308292319ae7cea7b1285e
Jailson Junior <jailsonjunior955@gmail.com>
2022-09-11 17:40:41 -0300

    First component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,18 @@
+import NotificationButton from "./components/NotificationButton"
 
 
 function App() {
 return(
+  <>
   <h1>Ola mundo</h1>
-)
+  <NotificationButton />
+  <NotificationButton />
+
+
+</>
+
+  )
+
 
 
 }
